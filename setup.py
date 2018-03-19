c2e3b7703ba38e06b0138e43b78318e91f8fb02a
Crt Mori <cmo@melexis.com>
2018-03-19 10:56:45 +0100

    Changing back to GPL license since we want contributions

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Sphinx :: Extension',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
