ba49c3b79d73a765ce4ad8a7ba666c25f6fb901a
Crt Mori <cmo@melexis.com>
2018-06-05 10:50:18 +0200

    Adding simple unit tests

--- tests/test_coverity.py
+++ tests/test_coverity.py
@@ -6,6 +6,12 @@ except ImportError as err:
     from mock import MagicMock, patch
     # from mock import call
 import mlx.coverity as cov
+import mlx.coverity_services as covservices
+
+# For Coverity - SOAP
+from suds.client import Client
+from suds.wsse import Security, UsernameToken
+from lxml import objectify
 
 
 class TestCoverity(TestCase):
@@ -13,8 +19,85 @@ class TestCoverity(TestCase):
     def SetUp(self):
         ''' SetUp to be run before each test to provide clean working env '''
 
-    @patch('mlx.coverity_services', autospec=True)
-    def test_dummy(self, cov_mock):
-        ''' Currently just a dummy unit test '''
-        cov_mock_object = MagicMock(spec=cov.CoverityConfigurationService)
-        cov_mock.return_value = cov_mock_object
+    @patch('mlx.coverity_services.UsernameToken')
+    @patch('mlx.coverity_services.Security')
+    @patch('mlx.coverity_services.Client')
+    def test_configuration_service_login(self, suds_client_mock, suds_security_mock, suds_username_mock):
+        ''' Test login function of CoverityConfigurationService '''
+        suds_client_mock.return_value = MagicMock(spec=Client)
+        suds_client_mock.return_value.service = MagicMock(spec=covservices.Service)
+        suds_client_mock.return_value.service.getVersion = MagicMock()
+        suds_security_mock.return_value = MagicMock(spec=Security)
+        suds_security_mock.return_value.tokens = []
+        suds_username_mock.return_value = MagicMock(spec=UsernameToken, return_value="bljah")
+
+        # Login to Coverity and obtain stream information
+        coverity_conf_service = cov.CoverityConfigurationService('http', 'scan.coverity.com', '8080')
+        suds_client_mock.assert_called_once_with('http://scan.coverity.com:8080/ws/v9/configurationservice?wsdl')
+
+        coverity_conf_service.login('user', 'password')
+        suds_security_mock.assert_called_once()
+        suds_username_mock.assert_called_once_with('user', 'password')
+        # suds_security_mock.tokens.assert_called_once_with("bljah")
+        # suds_client_mock.set_options.assert_called_once_with(wsse=suds_security_mock)
+
+    @patch('mlx.coverity_services.UsernameToken')
+    @patch('mlx.coverity_services.Security')
+    @patch('mlx.coverity_services.Client')
+    def test_defect_service_login(self, suds_client_mock, suds_security_mock, suds_username_mock):
+        ''' Test login function of CoverityDefectService '''
+        suds_client_mock.return_value = MagicMock(spec=Client)
+        suds_client_mock.return_value.service = MagicMock(spec=covservices.Service)
+        suds_client_mock.return_value.service.getVersion = MagicMock()
+        suds_security_mock.return_value = MagicMock(spec=Security)
+        suds_security_mock.return_value.tokens = []
+        suds_username_mock.return_value = MagicMock(spec=UsernameToken, return_value="bljah")
+
+        # Login to Coverity and obtain stream information
+        coverity_conf_service = cov.CoverityConfigurationService('http', 'scan.coverity.com', '8080')
+        suds_client_mock.assert_called_once_with('http://scan.coverity.com:8080/ws/v9/configurationservice?wsdl')
+
+        # Test CoverityDefectService
+        coverity_service = cov.CoverityDefectService(coverity_conf_service)
+        suds_client_mock.assert_called_with('http://scan.coverity.com:8080/ws/v9/defectservice?wsdl')
+
+        coverity_service.login('user', 'password')
+        suds_security_mock.assert_called_once()
+        suds_username_mock.assert_called_once_with('user', 'password')
+
+    @patch('mlx.coverity_services.UsernameToken')
+    @patch('mlx.coverity_services.Security')
+    @patch('mlx.coverity_services.Client')
+    def test_defect_service_defects(self, suds_client_mock, suds_security_mock, suds_username_mock):
+        ''' Test login function of CoverityDefectService '''
+        suds_client_mock.return_value = MagicMock(spec=Client)
+        suds_client_mock.return_value.service = MagicMock(spec=covservices.Service)
+        suds_client_mock.return_value.service.getVersion = MagicMock()
+        with open('tests/defect_soap.xml', 'r') as xmlfile:
+            defect_soap = objectify.fromstring(xmlfile.read())
+        print("defect_soap:")
+        print(objectify.dump(defect_soap))
+        print("defect_soap.Body:")
+        print(defect_soap.Body["{http://ws.coverity.com/v9}getMergedDefectsForSnapshotScopeResponse"].totalNumberOfRecords)
+        suds_client_mock.return_value.service.getMergedDefectsForSnapshotScope = MagicMock(spec=defect_soap, return_value=defect_soap)
+        suds_client_mock.return_value.factory = MagicMock()
+        suds_security_mock.return_value = MagicMock(spec=Security)
+        suds_security_mock.return_value.tokens = []
+        suds_username_mock.return_value = MagicMock(spec=UsernameToken, return_value="bljah")
+
+        # Login to Coverity and obtain stream information
+        coverity_conf_service = cov.CoverityConfigurationService('http', 'scan.coverity.com', '8080')
+        suds_client_mock.assert_called_once_with('http://scan.coverity.com:8080/ws/v9/configurationservice?wsdl')
+
+        # Test CoverityDefectService
+        coverity_service = cov.CoverityDefectService(coverity_conf_service)
+        suds_client_mock.assert_called_with('http://scan.coverity.com:8080/ws/v9/defectservice?wsdl')
+
+        coverity_service.login('user', 'password')
+        suds_security_mock.assert_called_once()
+        suds_username_mock.assert_called_once_with('user', 'password')
+
+        defects = coverity_service.get_defects('projectname', 'somestream')
+        print("Defects:")
+        print(defects)
+        self.assertTrue(False)
