928376c189a57ac253d06e138a23c3f7330fabed
Crt Mori <crt@the-mori.com>
2023-02-10 14:36:22 +0100

    Adjust setup.py with the better requires limits

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 
 project_url = 'https://github.com/melexis/sphinx-coverity-extension'
 
-requires = ['Sphinx>=2.1', 'docutils', 'setuptools_scm', 'matplotlib', 'mlx.traceability', 'suds-py3',
+requires = ['Sphinx>=2.1,<7.0', 'docutils', 'setuptools_scm', 'matplotlib', 'mlx.traceability', 'suds-py3',
             'urlextract']
 
 
 setup(
     name='mlx.coverity',
-    setup_requires=['setuptools-scm>=6.0.0,<7.*'],
+    setup_requires=['setuptools-scm>=6.0.0,<7.0'],
     use_scm_version=True,
     url=project_url,
     license='GNU General Public License v3 (GPLv3)',
@@ -29,7 +29,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
