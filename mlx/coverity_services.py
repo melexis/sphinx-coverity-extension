8c020fc759bd32e40649d6c7042fa0da030ca6a0
JWM <jwm@melexis.com>
2024-07-10 17:49:16 +0200

    Delete Sevice class; add new functions to CoverityConfigurationService class that uses REST API

--- mlx/coverity_services.py
+++ mlx/coverity_services.py
@@ -4,17 +4,18 @@
 
 # General
 import csv
+import json
 import logging
 import re
 from urllib.error import URLError
+from urllib.parse import urljoin
 
-# For Coverity - SOAP
-from suds.client import Client
-from suds.wsse import Security, UsernameToken
+# For Coverity - REST API
+import requests
 
 # -- Default values -- and global settings
 
-DEFAULT_WS_VERSION = 'v9'
+DEFAULT_BASE_URL = 'http://localhost:8080/'
 
 # Coverity built in Impact statuses
 IMPACT_LIST = {'High', 'Medium', 'Low'}
@@ -69,169 +70,63 @@ def compare_strings(str_a, str_b):
         return True
     return False
 
-
-class Service:
-    '''
-    Basic endpoint Service
-    '''
-
-    def __init__(self, transport, hostname, port, ws_version=DEFAULT_WS_VERSION):
-        self.set_transport(transport)
-        self.set_hostname(hostname)
-        self.set_port(port)
-        self.set_ws_version(ws_version)
-        self.client = None
-
-    def set_transport(self, transport):
-        '''Set transport protocol'''
-        self.transport = transport
-
-    def get_transport(self):
-        '''Get transport protocol'''
-        return self.transport
-
-    def set_hostname(self, hostname):
-        '''Set hostname for service'''
-        self.hostname = hostname
-
-    def get_hostname(self):
-        '''Get hostname for service'''
-        return self.hostname
-
-    def set_port(self, port):
-        '''Set port for service'''
-        self.port = port
-
-    def get_port(self):
-        '''Get port for service'''
-        return self.port
-
-    def set_ws_version(self, ws_version):
-        '''Set WS version for service'''
-        self.ws_version = ws_version
-
-    def get_ws_version(self):
-        '''Get WS version for service'''
-        return self.ws_version
-
-    def get_service_url(self, path='', add_port=True):
-        '''Get Service url with given path'''
-        url = self.transport + '://' + self.hostname
-        if self.port and add_port:
-            url += ':' + self.port
-        if path:
-            url += path
-        return url
-
-    def get_ws_url(self, service):
-        '''Get WS url with given service'''
-        return self.get_service_url('/ws/' + self.ws_version + '/' + service + '?wsdl')
-
-    def login(self, username, password):
-        '''Login to Coverity using given username and password'''
-        security = Security()
-        token = UsernameToken(username, password)
-        security.tokens.append(token)
-        self.client.set_options(wsse=security)
-
-    def validate_presence(self, url, service_name):
-        '''Initializes the client attribute while validating the presence of the service'''
-        try:
-            self.client = Client(url)
-            logging.info("Validated presence of %s [%s]", service_name, url)
-        except URLError:
-            self.client = None
-            logging.critical("No such %s [%s]", service_name, url)
-            raise
-
-
-class CoverityConfigurationService(Service):
+class CoverityConfigurationService:
     '''
     Coverity Configuration Service (WebServices)
     '''
 
-    def __init__(self, transport, hostname, port, ws_version=DEFAULT_WS_VERSION):
-        super(CoverityConfigurationService, self).__init__(transport, hostname, port, ws_version)
+    def __init__(self, base_url=DEFAULT_BASE_URL):
+        self._base_url = base_url
+        self._version = "v2"
         self.checkers = None
-        url = self.get_ws_url('configurationservice')
-        logging.getLogger('suds.client').setLevel(logging.CRITICAL)
-        self.validate_presence(url, 'Coverity Configuration Service')
 
-    def login(self, username, password):
-        '''Login to Coverity Configuration service using given username and password'''
-        super(CoverityConfigurationService, self).login(username, password)
-        version = self.get_version()
-        if version is None:
-            raise RuntimeError("Authentication to [%s] FAILED for [%s] account - check password"
-                               % (self.get_service_url(), username))
+    @property
+    def base_url(self):
+        '''The base URL'''
+        return self._base_url
+
+    @base_url.setter
+    def base_url(self, value):
+        if not re.fullmatch(r"https?://.+:\d\d\d\d/?", value):
+            raise ValueError(f"Invalid base URL. Expected 'http(s)://<hostname>:<port>(/)'; Got {value}")
+        self._base_url = value
+
+    @property
+    def version(self):
+        '''The API version'''
+        return self._version
+
+    @property
+    def search_issues_url(self):
+        '''Get url to retrieve issues'''
+        return urljoin(self.base_url, f"/api/v2/issues/search")
+
+    @property
+    def column_keys_url(self):
+        '''Get url to retrieve all column keys'''
+        return urljoin(self.base_url, f"/api/v2/issues/columns?queryType=bySnapshot&retrieveGroupByColumns=true'")
+
+    def retrieve_issues(self, filters, username, password):
+        '''Retrieve the contents of the specified url in Coverity Connect.
+
+        Args:
+            filters (json): The filters as json
+            username (str): Username to log in
+            password (str): Password to log in
+        '''
+        req = requests.post(self.search_issues_url, json=filters, auth=(username, password))
+        if req.ok:
+            return json.loads(req.content)
         else:
-            logging.info("Authentication to [%s] using [%s] account was OK - version [%s]",
-                         self.get_service_url(), username, version.externalVersion)
+            return req.raise_for_status()
 
-    def get_version(self):
-        '''Get the version of the service, can be used as a means to validate access permissions'''
-        try:
-            return self.client.service.getVersion()
-        except URLError:
-            return None
-
-    @staticmethod
-    def get_project_name(stream):
-        '''Get the project name from the stream object'''
-        return stream.primaryProjectId.name
-
-    @staticmethod
-    def get_triage_store(stream):
-        '''Get the name of the triaging store from the stream object'''
-        return stream.triageStoreId.name
-
-    def get_stream(self, stream_name):
-        '''Get the stream object from the stream name'''
-        filter_spec = self.client.factory.create('streamFilterSpecDataObj')
-
-        # use stream name as an initial glob pattern
-        filter_spec.namePattern = stream_name
-
-        # get all the streams that match
-        streams = self.client.service.getStreams(filter_spec)
-
-        # find the one with a matching name
-        for stream in streams:
-            if compare_strings(stream.id.name, stream_name):
-                return stream
-        return None
-
-    # get a list of the snapshots in a named stream
-    def get_snapshot_for_stream(self, stream_name):
-        '''Get snapshot object for given stream name'''
-        stream_id = self.client.factory.create('streamIdDataObj')
-        stream_id.name = stream_name
-        # optional filter specification
-        filter_spec = self.client.factory.create('snapshotFilterSpecDataObj')
-        # return a list of snapshotDataObj
-        return self.client.service.getSnapshotsForStream(stream_id, filter_spec)
-
-    @staticmethod
-    def get_snapshot_id(snapshots, idx=1):
-        '''Get the nth snapshot (base 1) - minus numbers to count from the end backwards (-1 = last)'''
-        if bool(idx):
-            num_snapshots = len(snapshots)
-            if idx < 0:
-                required = num_snapshots + idx + 1
-            else:
-                required = idx
-
-            if abs(required) > 0 and abs(required) <= num_snapshots:
-                # base zero
-                return snapshots[required - 1].id
-        return 0
-
-    def get_snapshot_detail(self, snapshot_id):
-        '''Get detailed information about a single snapshot'''
-        snapshot = self.client.factory.create('snapshotIdDataObj')
-        snapshot.id = snapshot_id
-        # return a snapshotInfoDataObj
-        return self.client.service.getSnapshotInformation(snapshot)
+    def retrieve_column_keys(self, username, password):
+        '''Retrieves the set of column keys and associated display names'''
+        req = requests.get(self.column_keys_url, auth=(username, password))
+        if req.ok:
+            return json.loads(req.content)
+        else:
+            return req.raise_for_status()
 
     def get_checkers(self):
         '''Get a list of checkers from the service'''
