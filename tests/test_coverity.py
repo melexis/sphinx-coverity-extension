2544193122329ae28bc7507c296cdb93d5e17717
JWM <jwm@melexis.com>
2024-07-25 17:49:34 +0200

    Add test to retrieve checkers

--- tests/test_coverity.py
+++ tests/test_coverity.py
@@ -16,99 +16,27 @@ class TestCoverity(TestCase):
 
     @patch("mlx.coverity_services.requests")
     def test_session_login(self, mock_requests):
-        """Test "login" by  retrieving all column keys (CoverityDefectService)"""
+        """Test login function of CoverityDefectService"""
         mock_requests.return_value = MagicMock(spec=requests)
 
         # Get the base url
         coverity_conf_service = mlx.coverity_services.CoverityDefectService("https", "scan.coverity.com/")
         self.assertEqual("https://scan.coverity.com/api/v2", coverity_conf_service.base_url)
 
-        # Login to Coverity and obtain stream information
+        # Login to Coverity
         coverity_conf_service.login("user", "password")
         mock_requests.Session.assert_called_once()
 
-    @patch("mlx.coverity_services.UsernameToken")
-    @patch("mlx.coverity_services.Security")
-    @patch("mlx.coverity_services.Client")
-    def test_defect_service_login(self, suds_client_mock, suds_security_mock, suds_username_mock):
-        """Test login function of CoverityDefectService"""
-        suds_client_mock.return_value = MagicMock(spec=Client)
-        suds_client_mock.return_value.service = MagicMock(spec=covservices.CoverityDefectService)
-        suds_client_mock.return_value.service.getVersion = MagicMock()
-        suds_security_mock.return_value = MagicMock(spec=Security)
-        suds_security_mock.return_value.tokens = []
-        suds_username_mock.return_value = MagicMock(spec=UsernameToken, return_value="bljah")
+    @patch.object(mlx.coverity_services.requests.Session, 'get')
+    def test_retrieve_checkers(self, mock_get):
+        """Test retrieving checkers (CoverityDefectService)"""
+        coverity_conf_service = mlx.coverity_services.CoverityDefectService("https", "scan.coverity.com/")
 
-        # Login to Coverity and obtain stream information
-        coverity_conf_service = cov.CoverityDefectService("https", "scan.coverity.com")
-        suds_client_mock.assert_called_once_with("https://scan.coverity.com:8080/ws/v9/configurationservice?wsdl")
+        # Login to Coverity
+        coverity_conf_service.login("user", "password")
 
-        # Test CoverityDefectService
-        coverity_service = cov.CoverityDefectService(coverity_conf_service)
-        suds_client_mock.assert_called_with("https://scan.coverity.com:8080/ws/v9/defectservice?wsdl")
-
-        coverity_service.login("user", "password")
-        suds_security_mock.assert_called_once()
-        suds_username_mock.assert_called_once_with("user", "password")
-
-    @patch("mlx.coverity_services.UsernameToken")
-    @patch("mlx.coverity_services.Security")
-    @patch("mlx.coverity_services.Client")
-    def test_defect_service_defects(self, suds_client_mock, suds_security_mock, suds_username_mock):
-        """Test login function of CoverityDefectService"""
-        suds_client_mock.return_value = MagicMock(spec=Client)
-        suds_client_mock.return_value.service = MagicMock(spec=covservices.CoverityDefectService)
-        suds_client_mock.return_value.service.getVersion = MagicMock()
-        with open("tests/defect_soap.xml", "rb") as xmlfile:
-            defect_soap = objectify.fromstring(xmlfile.read())
-        suds_client_mock.return_value.service.getMergedDefectsForSnapshotScope = MagicMock(
-            spec=defect_soap, return_value=defect_soap
-        )
-        suds_client_mock.return_value.factory = MagicMock()
-        suds_security_mock.return_value = MagicMock(spec=Security)
-        suds_security_mock.return_value.tokens = []
-        suds_username_mock.return_value = MagicMock(spec=UsernameToken, return_value="bljah")
-
-        # Login to Coverity and obtain stream information
-        coverity_conf_service = cov.CoverityDefectService("https", "scan.coverity.com")
-        suds_client_mock.assert_called_once_with("https://scan.coverity.com:8080/ws/v9/configurationservice?wsdl")
-
-        # Test CoverityDefectService
-        coverity_service = cov.CoverityDefectService(coverity_conf_service)
-        suds_client_mock.assert_called_with("https://scan.coverity.com:8080/ws/v9/defectservice?wsdl")
-
-        coverity_service.login("user", "password")
-        suds_security_mock.assert_called_once()
-        suds_username_mock.assert_called_once_with("user", "password")
-
-        filters = {
-            "checker": None,
-            "impact": None,
-            "kind": None,
-            "classification": None,
-            "action": None,
-            "component": None,
-            "cwe": None,
-            "cid": None,
-        }
-        coverity_service.get_defects("projectname", "somestream", filters)
-
-    @patch("mlx.coverity_services.UsernameToken")
-    @patch("mlx.coverity_services.Security")
-    @patch("mlx.coverity_services.Client")
-    def test_configuration_service_login_no_username_error(
-        self, suds_client_mock, suds_security_mock, suds_username_mock
-    ):
-        """Test login function of CoverityDefectService when error occurs"""
-        suds_client_mock.return_value = MagicMock(spec=Client)
-        suds_client_mock.return_value.service = MagicMock(spec=covservices.CoverityDefectService)
-        suds_client_mock.return_value.service.getVersion = MagicMock()
-        suds_security_mock.return_value = MagicMock(spec=Security)
-        suds_security_mock.return_value.tokens = []
-
-        # Login to Coverity and obtain stream information
-        coverity_conf_service = cov.CoverityDefectService("https", "scan.coverity.com")
-        suds_client_mock.assert_called_once_with("https://scan.coverity.com:8080/ws/v9/configurationservice?wsdl")
-
-        suds_client_mock.side_effect = Exception((401, "Unauthorized"))
-        coverity_conf_service.login("", "")
+        with open("tests/column_keys.json", "rb") as content:
+            mock_get.return_value.content = content.read()
+        mock_get.return_value.ok = True
+        coverity_conf_service.retrieve_checkers()
+        mock_get.assert_called_once()
